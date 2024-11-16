0d5113ff3c95d06fc886bb2f2cdb53716b8d6a84
okahate <onkarkahate1210@gmail.com>
2024-11-16 17:55:32 +0530

    local vs class variable

--- src/main/java/Trial/TestClass.java
+++ src/main/java/Trial/TestClass.java
@@ -3,6 +3,7 @@ package Trial;
 public class TestClass {
     int a=0;
     public static void test(){
+        int a = 2;
         TestClass obj=new TestClass();
         System.out.println(obj.a);
     }
